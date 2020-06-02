9be0a995b47ba825d7d50c3e5e8c5c6ee1e37954
Jolisa Tran <jolisa.chan.store@gmail.com>
2020-06-02 09:51:58 -0400

    make it better

---
+++ models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, String, create_engine, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+
+database_path = os.environ.get('DATABASE_URL')
+if not database_path:
+    database_name = "herokutest"
+    database_path = "postgres://{}:{}@{}/{}".format('postgres','password','localhost:5432', database_name)
+    #set DATABASE_URL = "postgres://postgres:password@localhost:5432/herokutest"
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Person
+Have title and release year
+'''
+class Person(db.Model):  
+  __tablename__ = 'People'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  catchphrase = Column(String)
+
+  def __init__(self, name, catchphrase=""):
+    self.name = name
+    self.catchphrase = catchphrase
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'catchphrase': self.catchphrase}
